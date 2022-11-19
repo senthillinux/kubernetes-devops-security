0954b877aa3b10ceca7007498865d0599e282ae3
Senthil Kumar <msenthil.unix@gmail.com>
2022-11-19 17:53:04 +0530

    Trivy Scan with parallel

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -4,6 +4,7 @@ echo $dockerImageName
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light $dockerImageName
 docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
 
+chmod -R jenkins.jenkins trivy
 ## Trivy Scan result procsessing
 
 exit_code=$?
